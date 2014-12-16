a932500c68ac7c80dbbf68f5e8569a15524d822a
Patrick Heneise <patrick@fastmail.im>
2014-12-16 14:37:54 +0100

    inital static commit

--- grunt/concurrent.js
+++
@@ -1,6 +0,0 @@
-/** executes multiple tasks concurrently so its faster */
-module.exports = {
-	'generate-dist': [
-		// unused to make build more clear
-	],
-};
--- grunt/config.js
+++
@@ -1,8 +0,0 @@
-/* common configuration for all modules. */
-module.exports = {
-	src: 'src',
-	tmp: '.tmp',
-	dist: 'dist',
-	connectPort: 9600,
-	livereloadPort: 19600,
-};
